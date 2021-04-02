b08e4ca06f5b6cd2fd0894e5fd9889cdf02b9d8c
Zaytoun's Studio <ahmedzaytoun@Zaytouns.local>
2021-04-02 03:26:02 +0200

    fix userLogin

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -10,17 +10,19 @@ const authUser = asyncHandler(async (req, res) => {
   const user = await User.findOne({ email }).exec();
 
   if (user && (await user.matchPassword(password))) {
-    res.json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      isAdmin: user.isAdmin,
-      isBlackListed: user.isBlackListed,
-      token: generateToken(user._id),
-    });
-  } else if (user.isBlackListed) {
-    res.status(403);
-    throw new Error("User is BlackListed");
+    if (!user.isBlackListed) {
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        isBlackListed: user.isBlackListed,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(403);
+      throw new Error("User is BlackListed");
+    }
   } else {
     res.status(401);
     throw new Error("Invalid email or password");
